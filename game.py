6585624b420ed04f32abc71eac1f1e0d416eeff8
Kasra Rahimi <kasra.rahimi@polymtl.ca>
2022-09-05 00:57:18 -0400

    functional version, to be improved

--- game.py
+++ game.py
@@ -3,20 +3,32 @@ import pygame, planet
 def main():
     blue = (30, 42, 156)
     grey = (59, 59, 64)
-    WIDTH, HEIGHT = 16 * 40, 9 * 40
+    black = (0, 0, 0)
+    W, H = 16 * 40, 9 * 40
     pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((W, H))
     pygame.display.set_caption("Planet simulation")
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-    earth = planet.Planet(blue, 5.972e24, WIDTH/2, HEIGHT/2, radius=10)
-    moon = planet.Planet(grey, 7.348e22, WIDTH/2, HEIGHT/2 - 384.4e6, 1022)
+    scale = planet.Planet.scale
+    earth = planet.Planet(blue, 5.972e24, 0, 0, radius=30)
+    moon = planet.Planet(grey, 7.348e22, 0, 384.4e6, 1022)
 
     while 1:
+        screen.fill(black)
         for event in pygame.event.get():
             if event == pygame.QUIT:
                 pygame.display.quit()
                 pygame.quit()
-                exit()
+                quit()
+        for entity in planet.Planet.planets:
+            x = entity.x * scale
+            y = entity.y * scale
+            pygame.draw.circle(screen, entity.color, (x, y), entity.radius)
+            entity.updatePosition()
+        
+        pygame.display.update()
+        clock.tick(30)
 
 if __name__ == '__main__':
     main()
