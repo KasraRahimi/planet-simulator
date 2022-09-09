ac02bf3fce061278ee1263f33f6f693b85e3defd
Kasra Rahimi <kasra.rahimi@polymtl.ca>
2022-09-09 14:46:07 -0400

    Planets till Mars, instead of just Earth-moon

--- game.py
+++ game.py
@@ -1,14 +1,27 @@
 import pygame, planet
 
-def realToGameCoor(orbitalPoints):
+# ~ simple color list ~
+blue = (0,0,255)
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+navy = (0, 0, 150)
+green = (0, 128, 0)
+grey = (128, 128, 128)
+yellow = (255,215,0)
+orange = (255,140,0)
+khaki = (240,230,140)
+# =====================
+
+def realToGameCoor(orbitalPoints, W, H):
     #  this function will take all the orbital
     #  points of a planet and output that same
     #  list as coordinates pygame can understand
     output = []
     scale = planet.Planet.scale
     for coordinate in orbitalPoints:
-        x = coordinate[0] * scale
-        y = coordinate[1] * scale
+        x = coordinate[0] * scale + W/2
+        y = coordinate[1] * scale + H/2
         output.append((x, y))
     return output
 
@@ -23,17 +36,59 @@ def main():
     pygame.display.set_caption("Planet simulation")
 
     scale = planet.Planet.scale
-    earth = planet.Planet(blue, 
-                        5.972e24, 
-                        W/(2*scale), 
-                        H/(2*scale), 
-                        radius=6371e3 * 10 * scale)
-    moon = planet.Planet(grey, 
-                        7.348e22, 
-                        W/(2*scale), 
-                        384.4e6 + H/(2*scale), 
-                        1022, 
-                        radius=1737e3 * 10 * scale)
+
+    sun = planet.Planet(
+        yellow,
+        1.989e30,
+        0,
+        0,
+        radius=15
+    )
+
+    mercury = planet.Planet(
+        grey,
+        33e22,
+        57.9e9,
+        0,
+        dy=47.9e3,
+        radius=3
+    )
+
+    venus = planet.Planet(
+        khaki,
+        4.87e24,
+        108.2e9,
+        0,
+        dy=36e3,
+        radius=5
+    )
+
+    earth = planet.Planet(
+        blue,
+        5.9736e24,
+        149.6e9,
+        0,
+        dy=29.8e3,
+        radius=6
+    )
+
+    mars = planet.Planet(
+        red,
+        642e21,
+        228e9,
+        0,
+        dy=24.1e3,
+        radius=4
+    )
+
+    jupiter = planet.Planet(
+        orange,
+        1898e24,
+        778.5e9,
+        0,
+        dy=13.1e3,
+        radius=10
+    )
 
     running = True
     while running:
@@ -42,16 +97,16 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         for entity in planet.Planet.planets:
-            x = entity.x * scale
-            y = entity.y * scale
+            x = entity.x * scale + W/2
+            y = entity.y * scale + H/2
             pygame.draw.circle(screen, entity.color, (x, y), entity.radius)
             if len(entity.orbit) > 1:
-                orbit = realToGameCoor(entity.orbit)
+                orbit = realToGameCoor(entity.orbit, W, H)
                 pygame.draw.lines(screen, entity.color, False, orbit, 1)
             entity.updatePosition()
         
         pygame.display.update()
-        clock.tick(30)
+        clock.tick(60)
 
 if __name__ == '__main__':
     main()
